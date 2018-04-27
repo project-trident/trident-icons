f9d06040e36bdfb7d8b05e3eb0e44729bc1e528b
Ken Moore <ken@ixsystems.com>
2018-04-27 08:29:10 -0400

    Add an install-local shell script. This can be used for automatically detecting the icon directory or can take an optional input dir for where to install the icon themes.

---
+++ install-local.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#Quick Script for installing icon themes directly on the local system
+#  INPUTS: $1: path to installation directory (optional - only use when preparing to make a package)
+
+install_dir="$1"
+icon_themes="la-capitaine la-capitaine-dark"
+
+#Determine the installation dir
+# XDG Environment Settings
+if [ -z "${install_dir}" ] ; then
+  if [ "0" = `id -u` ] ; then
+    if [ -z "${XDG_DATA_DIRS}" ] ; then
+      #Set the default XDG environment variable
+      XDG_DATA_DIRS="/share:/usr/share:/usr/local/share"
+    fi
+    # Scan for the icons dir in the system-level directories
+    for dir in `echo "${XDG_DATA_DIRS}" | sed 's|:| |g'`
+    do
+      if [ -d "${dir}/icons" ] ; then
+        install_dir="${dir}/icons"
+        break
+      fi
+    done
+  elif [ -n "${XDG_DATA_HOME}" ] ; then
+    install_dir="${XDG_DATA_HOME}/icons"
+  fi
+fi
+# Fallback - Home Dir
+if [ -z "${install_dir}" ] ; then
+  install_dir="$HOME/.local/share/icons"
+fi
+# Start installing the icon themes
+echo "Installing Icon Themes to: ${install_dir}"
+if [ ! -d "${install_dir}" ] ; then
+  #Create the install dir first
+  mkdir -p "${install_dir}"
+fi
+for theme in ${icon_themes}
+do
+  echo " - Install Theme: ${theme}"
+  if [ -d "${install_dir}/${theme}" ] ; then
+    rm -rf "${install_dir}/${theme}"
+  fi
+  cp -R "${theme}" "${install_dir}/${theme}"
+done
+echo "Done"
